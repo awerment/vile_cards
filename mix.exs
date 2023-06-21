3a281118b21f4c4883e3bb9c178a262314887c08
Anatolij <awerment@gmail.com>
2023-06-22 01:34:54 +0200

    Chore: reorder dependencies

--- mix.exs
+++ mix.exs
@@ -32,18 +32,20 @@ defmodule VileCards.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
+      {:jason, "~> 1.2"},
       {:phoenix, "~> 1.7.6"},
       {:phoenix_html, "~> 3.3"},
-      {:phoenix_live_reload, "~> 1.2", only: :dev},
-      {:phoenix_live_view, "~> 0.19.0"},
-      {:floki, ">= 0.30.0", only: :test},
       {:phoenix_live_dashboard, "~> 0.8.0"},
-      {:esbuild, "~> 0.7", runtime: Mix.env() == :dev},
-      {:tailwind, "~> 0.2.0", runtime: Mix.env() == :dev},
+      {:phoenix_live_view, "~> 0.19.0"},
+      {:plug_cowboy, "~> 2.5"},
       {:telemetry_metrics, "~> 0.6"},
       {:telemetry_poller, "~> 1.0"},
-      {:jason, "~> 1.2"},
-      {:plug_cowboy, "~> 2.5"}
+      # dev
+      {:esbuild, "~> 0.7", runtime: Mix.env() == :dev},
+      {:phoenix_live_reload, "~> 1.2", only: :dev},
+      {:tailwind, "~> 0.2.0", runtime: Mix.env() == :dev},
+      # test
+      {:floki, ">= 0.30.0", only: :test}
     ]
   end
 
