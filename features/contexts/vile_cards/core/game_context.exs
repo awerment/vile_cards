1febcf2ecf1a4fbc55e80ab03e6a26ee59bb31b8
Anatolij <awerment@gmail.com>
2023-07-12 00:32:09 +0200

    Add BDD test for new game

--- features/contexts/vile_cards/core/game_context.exs
+++ features/contexts/vile_cards/core/game_context.exs
@@ -12,8 +12,18 @@ defmodule VileCards.Core.GameContext do
   @dummy_black_cards 1..100
   @dummy_white_cards 1..1000
 
+  given_ ~r/^an empty state$/, fn state -> {:ok, state} end
+
   given_ ~r/^a new game$/, fn state ->
     {:ok, Map.put(state, :game, Game.new(@game_id, @game_admin, @dummy_black_cards, @dummy_white_cards))}
+  end
+
+  when_ ~r/^a new game is started$/, fn state, %{table_data: [params]} ->
+    black_cards = params[:black_cards] |> String.split(",", trim: true) |> Enum.map(&String.trim/1)
+    white_cards = params[:white_cards] |> String.split(",", trim: true) |> Enum.map(&String.trim/1)
+    game = Game.new(params[:game_id], {params[:admin_id], params[:admin_name]}, black_cards, white_cards)
+
+    {:ok, Map.put(state, :game, game)}
   end
 
   when_ ~r/^a player with id "(?<id>[^"]+)" and name "(?<name>[^"]+)" joins$/,
@@ -25,6 +35,55 @@ defmodule VileCards.Core.GameContext do
     fn state, %{id: id} ->
       {:ok, Map.replace_lazy(state, :game, fn game -> Game.player_leave(game, id) end)}
     end
+
+  then_ ~r/^a new game with id "(?<game_id>[^"]+)" exists$/, fn state, %{game_id: game_id} ->
+    assert %Game{id: ^game_id} = state.game
+    {:ok, state}
+  end
+
+  then_ ~r/^the deck has (?<black_cards>[0-9]+) black cards on the draw pile$/, fn state, %{black_cards: black_cards} ->
+    {black_cards, ""} = Integer.parse(black_cards)
+    assert %Game{black: {draw, _discard}} = state.game
+    assert Enum.count(draw) == black_cards
+    {:ok, state}
+  end
+
+  then_ ~r/^the deck has (?<black_cards>[0-9]+) black cards on the discard pile$/, fn state, %{black_cards: black_cards} ->
+    {black_cards, ""} = Integer.parse(black_cards)
+    assert %Game{black: {_draw, discard}} = state.game
+    assert Enum.count(discard) == black_cards
+    {:ok, state}
+  end
+
+  then_ ~r/^the deck has (?<white_cards>[0-9]+) white cards on the draw pile$/, fn state, %{white_cards: white_cards} ->
+    {white_cards, ""} = Integer.parse(white_cards)
+    assert %Game{white: {draw, [] = _discard}} = state.game
+    assert Enum.count(draw) == white_cards
+    {:ok, state}
+  end
+
+  then_ ~r/^the deck has (?<white_cards>[0-9]+) white cards on the discard pile$/, fn state, %{white_cards: white_cards} ->
+    {white_cards, ""} = Integer.parse(white_cards)
+    assert %Game{white: {_draw, discard}} = state.game
+    assert Enum.count(discard) == white_cards
+    {:ok, state}
+  end
+
+  then_ ~r/^the game round is (?<round>[0-9]+)$/, fn state, %{round: round} ->
+    {round, ""} = Integer.parse(round)
+    assert %Game{round: ^round} = state.game
+    {:ok, state}
+  end
+
+  then_ ~r/^no czar is picked$/, fn state ->
+    assert %Game{czar: nil} = state.game
+    {:ok, state}
+  end
+
+  then_ ~r/^no black card is drawn$/, fn state ->
+    assert %Game{card: nil} = state.game
+    {:ok, state}
+  end
 
   then_ ~r/^a player with id "(?<id>[^"]+)" and name "(?<name>[^"]+)" exists$/,
     fn %{game: game} = state, %{id: id, name: name} ->
--- features/core_game.feature
+++ features/core_game.feature
@@ -1,5 +1,20 @@
 Feature: Game
 
+  Scenario: Creating a new game
+    Given an empty state
+    When a new game is started
+    | game_id | admin_id | admin_name | black_cards | white_cards   |
+    | game-id | admin-id | admin-name | 1, 2, 3     | 1, 2, 3, 4, 5 |
+    Then a new game with id "game-id" exists
+    And a player with id "admin-id" and name "admin-name" exists
+    And the deck has 3 black cards on the draw pile
+    And the deck has 0 black cards on the discard pile
+    And the deck has 5 white cards on the draw pile
+    And the deck has 0 white cards on the discard pile
+    And the game round is 0
+    And no czar is picked
+    And no black card is drawn
+
   Scenario: Players joining a game are added to the players map
     Given a new game
     When a player with id "id-1" and name "name-1" joins
